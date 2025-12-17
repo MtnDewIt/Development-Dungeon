6ce8d431f949a5f478ac5ad645bebdf733b75d41
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2025-12-18 04:49:23 +1100

    I fixed it :/

---
+++ BonoboTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Resources;
+
+[assembly: AssemblyTitle("BonoboTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BonoboTest")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("MtnDewIt ™ 2025")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.890.0.0")]
+[assembly: AssemblyFileVersion("1.890.0.0")]
+
+[assembly: AssemblyAssociatedContentFile("menus.bonobo.xml")]
+[assembly: AssemblyAssociatedContentFile("plugins.assetbrowser.xml")]
+[assembly: AssemblyAssociatedContentFile("plugins.bonobo.xml")]
+[assembly: AssemblyAssociatedContentFile("plugins.console.xml")]
+[assembly: AssemblyAssociatedContentFile("plugins.matman.xml")]
+[assembly: AssemblyAssociatedContentFile("plugins.scenelist.xml")]
+[assembly: AssemblyAssociatedContentFile("plugins.world.xml")]
+[assembly: AssemblyAssociatedContentFile("plugins.worldsource.xml")]
---
+++ NormalPluginTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("NormalPluginTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NormalPluginTest")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("MtnDewIt ™ 2025")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("e89b6be3-f8eb-40ef-9ece-238a7671d386")]
+
+[assembly: AssemblyVersion("1.890.0.0")]
+[assembly: AssemblyFileVersion("1.890.0.0")]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
