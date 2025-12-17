8c1b008520d9284b2769fd1db727f3da59456a18
MtnDewIt <107611247+MtnDewIt@users.noreply.github.com>
2025-12-17 20:48:06 +1100

    Add Test Executable

---
+++ BonoboTest/App.xaml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.Windows;
+
+namespace Bonobo.Application 
+{
+    public partial class App : System.Windows.Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+        }
+    }
+}
---
+++ BonoboTest/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+using System.IO;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace Bonobo.Application
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
